8279de9cfc31e20c0a65b2b6c0823d908405e727
thisisaaronland <devnull@localhost>
2018-11-01 12:19:45 -0700

    notes about PNG handling

--- seed/png.go
+++ seed/png.go
@@ -53,4 +53,67 @@ func SeedPNG(t slippy.Tile, c cache.Cache, nz_opts *nextzen.Options) (io.ReadClo
 	png_fh := ioutil.NopCloser(r)
 
 	return c.Set(png_key, png_fh)
+
+	// it might seem weird and inefficient to be starting from SeedGeoJSON
+	// rather than SeedSVG() and it probably is but the way the tile/*.go code
+	// is written now coupled with the fact that oksvg doesn't accept io.Reader
+	// thingies (only files) coupled with the fact that maybe you don't _want_
+	// to have a non-ephemeral cache of the SVG renders (dunno, that's your
+	// business) means that the code to work straight off an SVG cache looks
+	// like the code that follows... so for now, we don't (20181101/thisisaaronland)
+
+	/*
+
+		svg_fh, err := SeedSVG(t, c, nz_opts)
+
+		if err != nil {
+			return nil, err
+		}
+
+		defer svg_fh.Close()
+
+		tmpfile, err := ioutil.TempFile("", "svg")
+
+		defer func() {
+
+			_, err := os.Stat(tmpfile.Name())
+
+			if !os.IsNotExist(err) {
+				os.Remove(tmpfile.Name())
+			}
+		}()
+
+		_, err = io.Copy(tmpfile, svg_fh)
+
+		if err != nil {
+			return nil, err
+		}
+
+		tmpfile.Close()
+
+		// this doesn't even exist and would be in tile/geojson/go
+		// which is... weird
+
+		im, err := tile.ToImageFromSVG(tmpfile.Name())
+
+		if err != nil {
+			return nil, err
+		}
+
+		var buf bytes.Buffer
+		png_wr := bufio.NewWriter(&buf)
+
+		err = png.Encode(png_wr, im)
+
+		if err != nil {
+			return nuil, err
+		}
+
+		png_wr.Flush()
+
+		r := bytes.NewReader(buf.Bytes())
+		png_fh := ioutil.NopCloser(r)
+
+		return c.Set(png_key, png_fh)
+	*/
 }
