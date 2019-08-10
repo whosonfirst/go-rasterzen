33518eb42719b4aa5f15437a7643336dbe6b01ae
thisisaaronland <devnull@localhost>
2019-08-09 18:03:05 -0700

    first cut at addressing issue #12 and issue #13

---
+++ seed/catalog/catalog.go
@@ -0,0 +1,11 @@
+package catalog
+
+import (
+	"github.com/go-spatial/geom/slippy"	
+)
+
+type SeedCatalog interface {
+	LoadOrStore(string, slippy.Tile) error
+	Remove(string) error
+	Range(func(key, value interface{}) bool)
+}
---
+++ seed/catalog/sync.go
@@ -0,0 +1,37 @@
+package catalog
+
+import(
+	"sync"
+	"github.com/go-spatial/geom/slippy"	
+)
+
+type InMemorySeedCatalog struct {
+	SeedCatalog
+	seed_catalog *sync.Map
+}
+
+func NewInMemorySeedCatalog() (SeedCatalog, error) {
+
+	seed_catalog := new(sync.Map)
+
+	m := InMemorySeedCatalog{
+		seed_catalog: seed_catalog,
+	}
+
+	return &m, nil
+}
+
+func (m *InMemorySeedCatalog) LoadOrStore(k string, t slippy.Tile) error {
+	m.seed_catalog.LoadOrStore(k, t)
+	return nil
+}
+
+func (m *InMemorySeedCatalog) Remove(k string) error {
+	m.seed_catalog.Delete(k)
+	return nil
+}
+
+func (m *InMemorySeedCatalog) Range(f func(key, value interface{}) bool) {
+	m.seed_catalog.Range(f)
+}
+
